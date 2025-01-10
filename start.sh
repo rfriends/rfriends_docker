5438363f94c1d2b41e25cf91a817a7f7bf957b14
mapi <rfriends2017@ymail.ne.jp>
2025-01-10 23:47:00 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-hn=`hostname -i`:8000
-echo $hn > rfriends3/rfriends3_server.txt
-#cat rfriends3/rfriends3_server.txt
-#sh -c "cat /etc/*-release"
+#
+sh -c "cat /etc/*-release"
 cron &
 atd &
-sh rfriends3/rfriends3_server.sh 
-echo server exit
+echo server 
+exit
