653e025dbe0f9631647e40c305ada9d31375dd8f
mapi <rfriends2017@ymail.ne.jp>
2025-01-12 18:05:57 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 hn=`hostname -i`:8000
 HOME=/home/ubuntu
+#
 echo $hn > $HOME/rfriends3/rfriends3_server.txt
 #cat $HOME/rfriends3/rfriends3_server.txt
 #sh -c "cat /etc/*-release"
+#
 service cron start
 service atd start
 service lighttpd start
-
+# don't exit
+while :; do sleep 10; done
+#
+# built in web server
 #sh $HOME/rfriends3/rfriends3_server.sh 
 echo server exit
