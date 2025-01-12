3ad3ee674221bf3fbae9b0cafcf7c0d36e75c935
mapi <rfriends2017@ymail.ne.jp>
2025-01-12 16:42:25 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 hn=`hostname -i`:8000
-echo $hn > rfriends3/rfriends3_server.txt
-#cat rfriends3/rfriends3_server.txt
+HOME=/home/ubuntu
+echo $hn > $HOME/rfriends3/rfriends3_server.txt
+#cat $HOME/rfriends3/rfriends3_server.txt
 #sh -c "cat /etc/*-release"
 service cron start
 service atd start
 service lighttpd start
 
-#sh rfriends3/rfriends3_server.sh 
+#sh $HOME/rfriends3/rfriends3_server.sh 
 echo server exit
