3dc8441cb225cec1cb86425308e07b2228761a88
mapi <rfriends2017@ymail.ne.jp>
2025-01-12 18:54:18 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,15 +2,15 @@
 hn=`hostname -i`:8000
 HOME=/home/ubuntu
 #
-service cron start
-service atd start
+#service cron start
+#service atd start
 #
 # built in web server
 #echo $hn > $HOME/rfriends3/rfriends3_server.txt
 #sh $HOME/rfriends3/rfriends3_server.sh
 #
 # lighttpd
-service lighttpd start
+#service lighttpd start
 # don't exit
 while :; do sleep 10; done
 #
