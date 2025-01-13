b34a55258dc59f27247112ff80340f82f9c2d437
mapi <rfriends2017@ymail.ne.jp>
2025-01-13 09:02:56 +0900

    Update ex_docker2.sh

--- ex_docker2.sh
+++ ex_docker2.sh
@@ -2,5 +2,21 @@
 #
 # for Dockerfile2
 #
-mkdir $HOME/share
-docker run -d -it --rm --name ex-rfriends3-2 --mount type=bind,src=$HOME/share,target=/tmp/share rfriends3-2 /bin/bash
+#　ホスト： $HOME/share
+#　コンテナ： /tmp/share
+#
+#user=`whoami`
+#HOME=/home/$user
+hostdir=$HOME/share
+contdir=/tmp/share
+#
+if [ ! -d $hostdir ]; then
+  mkdir $hostdir
+fi
+#　コンテナ作成
+docker create --name ex-rfriends3-2 --mount type=bind,src=$hostdir,target=$contdir rfriends3-2
+#　コンテナスタート
+docker start ex-rfriends3-2
+#　アタッチ
+docker attach ex-rfriends3-2
+#
