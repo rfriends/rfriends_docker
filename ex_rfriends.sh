f7b72212dc6f02e8928b7e5f4929a456b9d523e9
mapi <rfriends2017@ymail.ne.jp>
2025-01-15 22:45:52 +0900

    Update ex_rfriends.sh

--- ex_rfriends.sh
+++ ex_rfriends.sh
@@ -9,16 +9,29 @@ hostdir=$HOME/share
 contdir=/tmp/share
 contname=rfriends3
 imgname=rfriends3
-port=8000
+portfw=no
 #
 if [ ! -d $hostdir ]; then
   mkdir $hostdir
+fi
+if [ -f $HOME/rfriends_port ]; then
+  port=`cat $HOME/rfriends_port`
+else
+  port=8000
 fi
 #　コンテナ削除
 docker stop $contname
 docker rm   $contname
 #　コンテナ作成
-docker create -p $port:$port -it --name $contname --mount type=bind,src=$hostdir,target=$contdir $imgname
+if [ $portfw = "yes" ]; then
+  echo "port forwarding = yes"
+  echo `hostname -i`:$port
+  docker create -p $port:$port -it --name $contname --mount type=bind,src=$hostdir,target=$contdir $imgname
+else
+  echo "port forwarding = no"
+  cat $HOME/rfriends_server.txt
+  docker create -it --name $contname --mount type=bind,src=$hostdir,target=$contdir $imgname
+fi
 #　コンテナスタート
 docker run $contname
 #
