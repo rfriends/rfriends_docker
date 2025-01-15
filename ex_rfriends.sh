6c2522bfdb58bbb346ee25f4785ea626b2d0d8ae
mapi <rfriends2017@ymail.ne.jp>
2025-01-15 21:39:42 +0900

    Update ex_rfriends.sh

--- ex_rfriends.sh
+++ ex_rfriends.sh
@@ -17,9 +17,7 @@ fi
 docker stop $contname
 docker rm   $contname
 #　コンテナ作成
-docker create -it --name $contname --mount type=bind,src=$hostdir,target=$contdir $imgname
+docker create -p 8000:8000 -it --name $contname --mount type=bind,src=$hostdir,target=$contdir $imgname
 #　コンテナスタート
-docker start $contname
-#　アタッチ
-docker attach $contname
+docker run $contname
 #
