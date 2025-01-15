591632eca92ece806efa29d69fc2b19e352b141f
mapi <rfriends2017@ymail.ne.jp>
2025-01-15 23:02:14 +0900

    Update ex_rfriends.sh

--- ex_rfriends.sh
+++ ex_rfriends.sh
@@ -14,22 +14,20 @@ portfw=no
 if [ ! -d $hostdir ]; then
   mkdir $hostdir
 fi
-if [ -f $HOME/rfriends_port ]; then
-  port=`cat $HOME/rfriends_port`
-else
-  port=8000
-fi
+# ポート番号は変更不可
+port=8000
+
 #　コンテナ削除
 docker stop $contname
 docker rm   $contname
 #　コンテナ作成
 if [ $portfw = "yes" ]; then
   echo "port forwarding = yes"
-  echo `hostname -i`:$port
+  echo "ホストのIPアドレス:$port でアクセスしてください"
   docker create -p $port:$port -it --name $contname --mount type=bind,src=$hostdir,target=$contdir $imgname
 else
   echo "port forwarding = no"
-  cat $HOME/rfriends_server.txt
+  echo "コンテナのIPアドレス:$port でアクセスしてください"
   docker create -it --name $contname --mount type=bind,src=$hostdir,target=$contdir $imgname
 fi
 #　コンテナスタート
