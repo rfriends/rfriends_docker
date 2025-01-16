ece9b5c20244256d0c09d81a12a0ab7c07bffd5d
mapi <rfriends2017@ymail.ne.jp>
2025-01-16 10:13:48 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 #
+contshare1=/tmp/share/usr2
+contshare2=/home/$user/rfriends3/config
+#
 if [ ! -d $contshare1 ]; then
   mkdir -p $contshare1
   echo mkdir -p $contshare1
@@ -7,22 +10,20 @@ fi
 #
 cat <<EOF > $contshare2/usrdir.ini
 usrdir = "$contshare1"
-tmpdir = "$chome/tmp/"
+tmpdir = "/home/$user/tmp/"
 EOF
 
 hn=`hostname -i`:$port
 echo "container IPaddress : $hn"
-echo $hn > $chome/rfriends3/rfriends3_server.txt
+echo $hn > /home/$user/rfriends3/rfriends3_server.txt
 #
 sudo service cron start
 sudo service atd start
 #
-# builtin web server
-#sh $HOME/rfriends3/rfriends3_server.sh
-#
 # lighttpd web server
 sudo service lighttpd start
+
 # don't exit
-while :; do sleep 10; done
+while true; do sleep 10; done
 #
 echo server exit
