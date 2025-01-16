62b385f4d21ad5a44a40a18e6f39c1dae81d58c6
mapi <rfriends2017@ymail.ne.jp>
2025-01-16 19:21:44 +0900

    Add files via upload

---
+++ test/start.sh
@@ -0,0 +1,31 @@
+
+#!/bin/bash
+#
+contshare1=/tmp/share/usr2
+contshare2=/home/$user/rfriends3/config
+#
+if [ ! -d $contshare1 ]; then
+  mkdir -p $contshare1
+fi
+#
+cat <<EOF > $contshare2/usrdir.ini
+usrdir = "$contshare1"
+tmpdir = "/home/$user/tmp/"
+EOF
+
+hn=`hostname -i`:$port
+echo $hn > /home/$user/rfriends3/rfriends3_server.txt
+#
+sudo service cron start
+sudo service atd start
+#
+# lighttpd web server
+sudo service lighttpd start
+
+echo
+echo "container IPaddress : $hn"
+
+# don't exit
+while true; do sleep 10; done
+#
+echo server exit
