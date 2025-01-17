61c2df02e362b9d1a8398517f0b5966a569aa96c
mapi <rfriends2017@ymail.ne.jp>
2025-01-17 10:22:37 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 #
 contshare1=/home/$user/smbdir/usr2
@@ -9,12 +8,14 @@ if [ ! -d $contshare1 ]; then
 fi
 #
 cat <<EOF > $contshare2/usrdir.ini
-usrdir = "$contshare1"
+usrdir = "$contshare1/"
 tmpdir = "/home/$user/tmp/"
 EOF
 
+rfriends3dir=/home/$user/rfriends3
+
 hn=`hostname -i`:$port
-echo $hn > /home/$user/rfriends3/rfriends3_server.txt
+echo $hn > $rfriends3dir/rfriends3_server.txt
 #
 sudo service cron start
 sudo service atd start
@@ -28,6 +29,41 @@ sudo service smbd start
 echo
 echo "container IPaddress : $hn"
 date
+
+echo
+echo ----- recovery start
+echo
+
+# recovery crontab
+if [ -f $contshare2/crontab.conf ]; then
+   crontab -u $user $contshare2/crontab.conf
+   echo "crontabを復元しました。"
+fi
+
+# def
+echo $contshare1."/"    > $rfriends3dir/etc/usrdef 
+echo "/home/$user/tmp/" > $rfriends3dir/etc/tmpdef 
+
+# daily process
+# opt 1:on
+#
+# ラジコ予約
+# らじる予約
+# タイムフリー録音
+# らじる聞き逃し録音
+# らじるゴガク録音
+# ポッドキャスト
+# クリーン処理
+opt="1,1,1,1,1,1,1"
+#
+fl="$rfriends3dir/script/ex_rfriends.sh"
+echo デイリー処理を開始しました
+sh $fl 0 $opt 
+echo デイリー処理を終了しました
+
+echo
+echo ----- recovery end
+echo
 
 # don't exit
 while true; do sleep 10; done
