2dd91991fc0a66d30caf58b267429ce2edd7c568
mapi <rfriends2017@ymail.ne.jp>
2025-01-19 13:26:14 +0900

    Update docker_start.sh

--- docker_start.sh
+++ docker_start.sh
@@ -14,7 +14,7 @@ EOF
 
 rfriends3dir=/home/$user/rfriends3
 
-host=`ipconfig | grep "inet " | grep -v "127.0.0.1"`
+host=`hostname -i`
 hn=`hostname`
 echo ${host}:${port} > $rfriends3dir/rfriends3_server.txt
 #
