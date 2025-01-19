3ee031ee156e02ba2d5fab770d336135ef0ac5cb
mapi <rfriends2017@ymail.ne.jp>
2025-01-19 13:39:14 +0900

    Update run_contena.sh

--- run_contena.sh
+++ run_contena.sh
@@ -33,10 +33,6 @@ docker stop $contname
 docker rm   $contname
 
 echo
-echo "http port forwarding = $httpfw $phttp" 
-echo "samba port forwarding = $smbfw $psmb" 
-
-echo
 echo "[ホスト共有]"
 echo "ユーザ  : $hostuser"
 echo "共有１ : $hostshare1"
@@ -47,6 +43,10 @@ echo "[コンテナ共有]"
 echo "ユーザ : $contuser"
 echo "共有１ : $contshare1"
 echo "共有２ : $contshare2"
+
+echo
+echo "http port forwarding = $httpfw $phttp" 
+echo "samba port forwarding = $smbfw $psmb" 
 
 echo 
 echo "host IPaddress :"
