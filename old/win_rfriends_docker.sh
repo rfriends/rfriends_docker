93f6d43256b156131623cd380c70cc8b26fd0893
mapi <rfriends2017@ymail.ne.jp>
2025-01-22 00:37:19 +0900

    Add files via upload

---
+++ old/win_rfriends_docker.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+#
+# コンテナ実行 for win
+# --------------------------------------------------------
+# コンテナとイメージ
+contname=rfriends3
+imgname=rfriends3
+
+# ポートフォワーディング
+# no にすると同一LANからアクセス不可だが複数のコンテナが実行可能
+# windows環境の場合は、例　smbfw=no or 8445:445
+httpfw=yes
+smbfw=no
+#
+phttp='-p 8000:8000'
+psmb='-p 445:445'
+
+hostip=`ipconfig | grep -a "IPv4" | cut -d ':' -f2`
+# --------------------------------------------------------
+# イメージ作成
+docker build ./ -t $imgname
+#
+export contname
+export imgname
+export httpfw
+export smbfw
+export phttp
+export psmb
+export hostip
+
+#
+sh run_contena.sh
+#
+echo
+echo exit rfriends_docker_win.sh
+exit 0
