a8a8abc69bfdfa859dd943598a0f537fdc831086
mapi <rfriends2017@ymail.ne.jp>
2026-07-22 05:17:04 +0900

    Add build command for WSL container

--- run_wsl_containers.bat
+++ run_wsl_containers.bat
@@ -2,5 +2,6 @@
 call load_env.bat
 mkdir -p %share_dir%\smbdir\usr2
 mkdir -p %share_dir%\rfriends3\config
+wslc build -t %image_name% . 
 
 wslc run -d --name %container_name% -p %http_port%:8000 -v "%share_dir%\smbdir\usr2:/home/user/smbdir/usr2" -v "%share_dir%\rfriends3\config:/home/user/rfriends3/config" %image_name%
