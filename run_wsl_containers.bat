14085bebdb817f2056dcd728ca34ed31f8ebb1b5
mapi <rfriends2017@ymail.ne.jp>
2026-07-22 04:02:33 +0900

    Update run_wsl_containers.bat

--- run_wsl_containers.bat
+++ run_wsl_containers.bat
@@ -1,5 +1,6 @@
 @echo off
-mkdir -p ./share/smbdir/usr2
-mkdri -p ./share/rfriends3/config
 call load_env.bat
+mkdir -p %share_dir%\smbdir\usr2
+mkdri -p %share_dir%\rfriends3\config
+
 wslc run -d --name %container_name% -p %http_port%:8000 -v "%share_dir%\smbdir\usr2:/home/user/smbdir/usr2" -v "%share_dir%\rfriends3\config:/home/user/rfriends3/config" %image_name%
