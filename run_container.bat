89ad508b23b7569cc5aae5e461e8cb7ddeddc837
mapi <rfriends2017@ymail.ne.jp>
2026-07-21 15:19:29 +0900

    Fix variable case in run_container.bat

--- run_container.bat
+++ run_container.bat
@@ -1,3 +1,3 @@
 @echo off
 call load_env.bat
-echo wslc run -d --name %COMPOSE_PROJECT_NAME% -p %http_port%:8000 -v "%SHAREDIR%smbdir\usr2:/home/user/smbdir/usr2" -v "%SHAREDIR%rfriends3\config:/home/user/rfriends3/config" %IMAGE%
+echo wslc run -d --name %COMPOSE_PROJECT_NAME% -p %http_port%:8000 -v "%share_dir%smbdir\usr2:/home/user/smbdir/usr2" -v "%share_dir%rfriends3\config:/home/user/rfriends3/config" %IMAGE%
