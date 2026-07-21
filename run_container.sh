2f3cc3f4c985112d355637499bb524e33be50ab9
mapi <rfriends2017@ymail.ne.jp>
2026-07-21 15:20:41 +0900

    Fix variable names in container run command

--- run_container.sh
+++ run_container.sh
@@ -3,4 +3,4 @@
 source ./load_env.sh
 
 # 2. コンテナの実行（dockerまたはpodmanを想定し、変数を展開してエコー）
-echo container run -d --name "${CONTAINER_NAME}" -p "${PORT}":8000 -v "${SHAREDIR}smbdir/usr2:/home/user/smbdir/usr2" -v "${SHAREDIR}rfriends3/config:/home/user/rfriends3/config" "${IMAGE}"
+echo container run -d --name "${CONTAINER_NAME}" -p "${http_port}":8000 -v "${share_dir}smbdir/usr2:/home/user/smbdir/usr2" -v "${shre_dir}rfriends3/config:/home/user/rfriends3/config" "${IMAGE}"
