ec2703c979475836fb737eae7dba59136f00a0ea
mapi <rfriends2017@ymail.ne.jp>
2026-07-22 04:53:36 +0900

    Update run_apple_container.sh

--- run_apple_container.sh
+++ run_apple_container.sh
@@ -2,5 +2,5 @@
 source ./load_env.sh
 mkdir -p ${share_dir}/smbdir/usr2
 mkdir -p ${share_dir}/rfriends3/config
-
+container build --platform linux/arm64 -t ${image_name} .
 container run -d --name "${container_name}" -p "${http_port}":8000 -v "${share_dir}/smbdir/usr2:/home/user/smbdir/usr2" -v "${share_dir}/rfriends3/config:/home/user/rfriends3/config" "${image_name}"
