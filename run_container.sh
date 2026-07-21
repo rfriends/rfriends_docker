429d8ae6d46c0c079ec2cc157657296d91bc5f6e
mapi <rfriends2017@ymail.ne.jp>
2026-07-21 15:17:17 +0900

    Update volume paths for container run command

--- run_container.sh
+++ run_container.sh
@@ -3,4 +3,4 @@
 source ./load_env.sh
 
 # 2. コンテナの実行（dockerまたはpodmanを想定し、変数を展開してエコー）
-echo container run -d --name "${CONTAINER_NAME}" -p "${PORT}":8000 -v "${SHAREDIR}smbdir/usr:/home/user/smbdir/usr" "${IMAGE}"
+echo container run -d --name "${CONTAINER_NAME}" -p "${PORT}":8000 -v "${SHAREDIR}smbdir/usr2:/home/user/smbdir/usr2" -v "${SHAREDIR}rfriends3/config:/home/user/rfriends3/config" "${IMAGE}"
