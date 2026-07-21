0dd7613643423c2692953a345933655f38d3f8dc
mapi <rfriends2017@ymail.ne.jp>
2026-07-21 15:28:42 +0900

    Fix command execution in run_wslc_container.bat

---
+++ run_wslc_container.bat
@@ -0,0 +1,3 @@
+@echo off
+call load_env.bat
+wslc run -d --name %COMPOSE_PROJECT_NAME% -p %http_port%:8000 -v "%share_dir%\smbdir\usr2:/home/user/smbdir/usr2" -v "%share_dir%\rfriends3\config:/home/user/rfriends3/config" %IMAGE%
