dee9ae6f55ede2d8a92a3bf1d2787d29dae63c1c
mapi <rfriends2017@ymail.ne.jp>
2026-07-22 02:58:16 +0900

    Fix variable names in run_apple_container.sh

--- run_apple_container.sh
+++ run_apple_container.sh
@@ -1,3 +1,3 @@
 #!/bin/zsh
 source ./load_env.sh
-container run -d --name "${CONTAINER_NAME}" -p "${http_port}":8000 -v "${share_dir}/smbdir/usr2:/home/user/smbdir/usr2" -v "${shre_dir}/rfriends3/config:/home/user/rfriends3/config" "${IMAGE}"
+container run -d --name "${container_name}" -p "${http_port}":8000 -v "${share_dir}/smbdir/usr2:/home/user/smbdir/usr2" -v "${shre_dir}/rfriends3/config:/home/user/rfriends3/config" "${image_name}"
