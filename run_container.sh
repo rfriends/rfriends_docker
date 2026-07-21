2642a241b2e561008b0470f0412ac2a86bcab3d0
mapi <rfriends2017@ymail.ne.jp>
2026-07-21 15:01:59 +0900

    Create run_container.sh for container execution
    Add script to run a container with environment variables

---
+++ run_container.sh
@@ -0,0 +1,6 @@
+#!/bin/zsh
+# 1. 設定ファイルをインクルード
+source ./load_env.sh
+
+# 2. コンテナの実行（dockerまたはpodmanを想定し、変数を展開してエコー）
+echo container run -d --name "${CONTAINER_NAME}" -p "${PORT}":8000 -v "${SHAREDIR}smbdir/usr:/home/user/smbdir/usr" "${IMAGE}"
