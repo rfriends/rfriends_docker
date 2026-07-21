2ad172793dfa75cce83f60bd7b7bf8c96a1da38c
mapi <rfriends2017@ymail.ne.jp>
2026-07-21 15:29:32 +0900

    Remove comments from run_aple_container.sh

--- run_container.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/zsh
-# 1. 設定ファイルをインクルード
-source ./load_env.sh
-
-# 2. コンテナの実行（dockerまたはpodmanを想定し、変数を展開してエコー）
-echo container run -d --name "${CONTAINER_NAME}" -p "${http_port}":8000 -v "${share_dir}/smbdir/usr2:/home/user/smbdir/usr2" -v "${shre_dir}/rfriends3/config:/home/user/rfriends3/config" "${IMAGE}"
