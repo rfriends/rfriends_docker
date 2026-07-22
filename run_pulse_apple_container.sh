5d14e5052890f0b928be199720d50f8b0de93574
mapi <rfriends2017@ymail.ne.jp>
2026-07-23 02:32:15 +0900

    Update run_pulse_apple_container.sh

--- run_pulse_apple_container.sh
+++ run_pulse_apple_container.sh
@@ -1,16 +1,18 @@
 #!/bin/zsh
+# 2026/07/23
 exit
 source ./load_env.sh
 mkdir -p ${share_dir}/smbdir/usr2
 mkdir -p ${share_dir}/rfriends3/config
 container build --platform linux/arm64 -t ${image_name} .
 
-load-module module-native-protocol-tcp auth-anonymous=1 port=${pulse_port} listen=127.0.0.1
-pulseaudio --start
+killall pulseaudio 2>/dev/null
+pulseaudio --daemon
 
 container run -d --name "${container_name}" \
  -p "${http_port}":8000 \
  -v "${share_dir}/smbdir/usr2:/home/user/smbdir/usr2" \
  -v "${share_dir}/rfriends3/config:/home/user/rfriends3/config" \
  -e "PULSE_SERVER=tcp:${host_ip_address}:${pulse_port}" \ 
+ -e -e PULSE_SINK=${pulse_sink} \
  "${image_name}"
