c51462528e47bda986f230c599d0d525861602d4
mapi <rfriends2017@ymail.ne.jp>
2026-07-23 02:28:07 +0900

    Refactor container run command formatting
    Updated container run command for better readability.

--- run_apple_container.sh
+++ run_apple_container.sh
@@ -1,8 +1,13 @@
 #!/bin/zsh
-#  動作しません。
-exit
+# 2026/07/23
+#
 source ./load_env.sh
 mkdir -p ${share_dir}/smbdir/usr2
 mkdir -p ${share_dir}/rfriends3/config
 container build --platform linux/arm64 -t ${image_name} .
-container run -d --name "${container_name}" -p "${http_port}":8000 -v "${share_dir}/smbdir/usr2:/home/user/smbdir/usr2" -v "${share_dir}/rfriends3/config:/home/user/rfriends3/config" "${image_name}"
+container run -d \
+ --name "${container_name}" \
+ -p "${http_port}":8000 \
+ -v "${share_dir}/smbdir/usr2:/home/user/smbdir/usr2" \
+ -v "${share_dir}/rfriends3/config:/home/user/rfriends3/config" \
+ "${image_name}"
