19563a7cfa759fd15962aef078eeb135583acb44
mapi <rfriends2017@ymail.ne.jp>
2026-07-22 20:27:18 +0900

    Update run_apple_container.sh

--- run_apple_container.sh
+++ run_apple_container.sh
@@ -3,6 +3,10 @@ source ./load_env.sh
 mkdir -p ${share_dir}/smbdir/usr2
 mkdir -p ${share_dir}/rfriends3/config
 container build --platform linux/arm64 -t ${image_name} .
+
+load-module module-native-protocol-tcp auth-anonymous=1 port=${pulse_port} listen=127.0.0.1
+pulseaudio --start
+
 container run -d --name "${container_name}" \
  -p "${http_port}":8000 \
  -v "${share_dir}/smbdir/usr2:/home/user/smbdir/usr2" \
